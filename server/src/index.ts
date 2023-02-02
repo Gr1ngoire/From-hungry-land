0e4843fda70a7a1fe7aabb26e53ee63885f12aea
Nazariy <nazar260804@gmail.com>
2023-02-03 01:07:17 +0200

    moved db init to source file

--- server/src/index.ts
+++ server/src/index.ts
@@ -3,15 +3,14 @@ import bodyParser from 'body-parser';
 import {dataSource} from './app-data-source'
 import {ENV} from "@/common/enums/enums";
 
-dataSource.initialize()
-    .then(() => {
-        console.log('Connected to db')
-    })
-    .catch((err) => {
-    console.log(err);
-})
+import {apiRouter} from "@/routers/routers";
+
+
 
 const app = express();
 app.use(bodyParser.urlencoded({extended: true}))
+
+app.use("/api", apiRouter);
+
 
 app.listen(ENV.APP.SERVER_PORT);
