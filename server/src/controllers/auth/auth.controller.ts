8a3098a23c00150f8344bdd2b38416547ade8f4b
wilhelm wesser <wilhelmwesser228@gmail.com>
2023-02-03 20:18:05 +0200

    Server works fine and body is properly parsed

--- server/src/controllers/auth/auth.controller.ts
+++ server/src/controllers/auth/auth.controller.ts
@@ -4,7 +4,16 @@ import {UserResponseDto} from "@/common/types/types";
 
 class AuthController {
     public signIn(req: Request<any, any, UserSignInDto>, res: Response<UserResponseDto>) {
-        console.log('sign in' ,req.body)
+        console.log('sign in', req.body)
+        res.send({
+            id: 1,
+            nickname: 'AAAAAAA',
+            email: 'Aboba',
+            role: {
+                id: 1,
+                name: 'BBBBBBBB'
+            }
+        })
     }
 
     public signUp(req: Request<any, any, UserSignInDto>, res: Response<UserResponseDto>) {
--- server/src/index.ts
+++ server/src/index.ts
@@ -1,12 +1,11 @@
 import express from 'express'
-import bodyParser from 'body-parser';
 import {ApiRoutes, ENV} from "@/common/enums/enums";
 import {apiRouter} from "@/routes/routes";
 
 
 const app = express();
 
-app.use(bodyParser.urlencoded({extended: true}))
+app.use(express.json())
 app.use(ApiRoutes.API, apiRouter)
 
 app.listen(ENV.APP.SERVER_PORT, () => {
