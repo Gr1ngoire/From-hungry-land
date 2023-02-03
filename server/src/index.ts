7bac81cc6a4c8c07f9461e25210465592d73f47b
wilhelm wesser <wilhelmwesser228@gmail.com>
2023-02-03 20:06:45 +0200

    Server works1

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,17 +1,14 @@
 import express from 'express'
 import bodyParser from 'body-parser';
 import {ApiRoutes, ENV} from "@/common/enums/enums";
-import {apiRouter} from "@/routers/routers";
+import {apiRouter} from "@/routes/routes";
+
 
 const app = express();
+
 app.use(bodyParser.urlencoded({extended: true}))
 app.use(ApiRoutes.API, apiRouter)
-app.get("/test", (req, res) => {
-    res.send('AAAAAAAAAAA')
-})
 
 app.listen(ENV.APP.SERVER_PORT, () => {
     console.log(`Listening port ${ENV.APP.SERVER_PORT}`)
 });
-
-app.listen(ENV.APP.SERVER_PORT);
