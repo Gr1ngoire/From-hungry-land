415750b8cd80d616b3505ac71606a682861ff259
tonujet <tonujet@gmail.com>
2023-02-03 02:50:57 +0100

    Add basic logic to RecipeController

--- server/src/controllers/recipe/recipe.controller.ts
+++ server/src/controllers/recipe/recipe.controller.ts
@@ -1,7 +1,25 @@
 import {recipeService} from "@/services/services";
+import {Request, Response} from "express";
+import RecipeQueryOptionInterface from "shared/common/interfaces/recipe.query.option/recipe.query.option.interface";
 
 class RecipeController{
     private readonly recipeService = recipeService;
+    async getRecipe (req: Request<{id:number}>, res: Response):Promise<void>{
+        try {
+            res.json(await this.recipeService.get(req.params.id))
+        }catch (e){
+            res.status(500).json(e.message)
+        }
+
+    }
+
+    async getAllRecipes (req: Request<any, any, any, RecipeQueryOptionInterface>, res: Response):Promise<void>{
+        try {
+            res.json(await this.recipeService.getAll(req.query))
+        }catch (e){
+            res.status(500).json(e.message)
+        }
+    }
 }
 
 export const recipeController = new RecipeController();
