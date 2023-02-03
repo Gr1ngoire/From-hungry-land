e8707d1b344ad91065b9ddb53302387519cd16c8
tonujet <tonujet@gmail.com>
2023-02-03 21:51:17 +0100

    Delete interfaces folder form shared folder

--- shared/common/interfaces/recipe.query.option/recipe.query.option.interface.ts
+++
@@ -1,9 +0,0 @@
-import {RecipeDifficulty} from "../../../../server/src/common/enums/recipe/recipe-difficulty.enum";
-
-export default interface RecipeQueryOptionInterface{
-    take?:number,
-    skip?:number,
-    searchQuery?:string
-    difficulty?:RecipeDifficulty[]
-}
-
