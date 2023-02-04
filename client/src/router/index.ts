1cbad396576c0523bee51011bc5cc1043fa20f2e
Nazariy <nazar260804@gmail.com>
2023-02-04 19:33:25 +0200

    added product list route

--- client/src/router/index.ts
+++ client/src/router/index.ts
@@ -1,14 +1,21 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHistory } from "vue-router";
+import { ClientRoutes } from "@/common/enums/enums";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
-     /* {
+    /* {
       path: '/example',
       name: 'example',
       component: ExampleComponent
     }, */
-  ]
-})
 
-export default router
+    {
+      path: ClientRoutes.PRODUCTS,
+      name: "products",
+      component: () => import("@/views/products/ProductListView.vue"),
+    },
+  ],
+});
+
+export default router;
