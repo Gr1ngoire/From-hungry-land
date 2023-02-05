6b7df2ccfc4fb6f61f8bd092eb977067fa65ae37
Nazariy <nazar260804@gmail.com>
2023-02-05 17:48:55 +0200

    created common views exports file

--- client/src/router/index.ts
+++ client/src/router/index.ts
@@ -1,5 +1,6 @@
 import { createRouter, createWebHistory } from "vue-router";
 import { ClientRoutes } from "@/common/enums/enums";
+import { ProductListView } from "@/views/views";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -13,7 +14,7 @@ const router = createRouter({
     {
       path: ClientRoutes.PRODUCTS,
       name: "products",
-      component: () => import("@/views/products/ProductListView.vue"),
+      component: ProductListView,
     },
   ],
 });
