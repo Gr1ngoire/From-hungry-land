2d6f848168049d7bfbedcd284a8e51732b1d483c
Nazariy <nazar260804@gmail.com>
2023-02-05 03:10:37 +0200

    added dropdown for filter in smaller screens

--- client/src/views/products/ProductListView.vue
+++ client/src/views/products/ProductListView.vue
@@ -5,12 +5,13 @@ import Product from "@/components/Product.vue";
 
 import { ref, reactive, onMounted } from "vue";
 import { useProductListStore } from "@/stores/productListStore.js";
-import type { ProductTagDTO, ProductDTO } from "@/common/types/types";
+import type { ProductDTO } from "@/common/types/types";
 
 const productsStore = useProductListStore();
 
 const filters = ref([] as string[]);
 const options = ref([] as ProductDTO[]);
+const condition = ref(false);
 
 onMounted(() => {
     productsStore.fetchProducts().then(() => {
@@ -19,7 +20,7 @@ onMounted(() => {
     productsStore.fetchProductTags().then(() => {
         filters.value = [...productsStore.getProductTags]
     });
-    
+
 });
 
 const selected = ref({
@@ -45,6 +46,9 @@ const removeFilter = (filter: string) => {
     chosenFilters.splice(chosenFilters.indexOf(filter), 1);
 }
 
+const toggleDropdown = () => {
+    condition.value = !condition.value;
+}
 
 </script>
 
@@ -52,10 +56,29 @@ const removeFilter = (filter: string) => {
     <div class="page-wrapper">
         <div class="page-section rounded">
             <p class="caption-text">Add Products</p>
+
             <div class="product-search-wrapper">
                 <VueSelect class="flex-grow-4 min-w-10" :options="options" v-model="selected" label="name" />
                 <Button text="Add" class="flex-grow-1" bgColor="bg-cyan-green" :icon="['fas', 'plus']" />
             </div>
+
+
+            <Button @click="toggleDropdown" class="filters-dropdown-btn flex-grow-1" text=" Filters"
+                bgColor="bg-cyan-green" :icon="['fas', 'caret-down']" />
+
+            <Transition name="slide-fade">
+                <div v-show="condition" class="filters-dropdown">
+                    <div class="filters-section">
+                        <Button v-for="filter in chosenFilters" :text="filter" class="flex-grow-1" bgColor="bg-pink"
+                            @click="removeFilter(filter)" />
+                    </div>
+                    <div class="filters-section">
+                        <Button v-for="filter in filters" :text="filter" class="flex-grow-1" bgColor="bg-grey"
+                            @click="addFilter(filter)" />
+                    </div>
+                </div>
+            </Transition>
+
             <p class="caption-text">Your Products</p>
             <div class="products-wrapper">
                 <Product v-for="product in productsStore.getProducts" :name="product.name" :image="product.imgUrl"
@@ -63,7 +86,7 @@ const removeFilter = (filter: string) => {
                 </Product>
             </div>
         </div>
-        <div class="page-section rounded">
+        <div class="page-section rounded filters-normal">
             <p class="caption-text">Filters</p>
             <div class="filters-section">
                 <Button v-for="filter in chosenFilters" :text="filter" class="flex-grow-1" bgColor="bg-pink"
@@ -130,13 +153,80 @@ const removeFilter = (filter: string) => {
     overflow-y: hidden;
 }
 
+.filters-dropdown {
+    display: none;
+    margin-bottom: 1rem;
+}
+
+.filters-dropdown-btn {
+    display: none;
+    margin-bottom: 1rem;
+}
+
+@media (max-width: 768px) {
+    .page-wrapper {
+        grid-template-columns: 1fr;
+    }
+
+    .filters-dropdown {
+        display: block;
+    }
+
+    .filters-normal {
+        display: none;
+    }
+
+    .filters-dropdown-btn {
+        display: block;
+    }
+
+    .products-wrapper {
+        display: flex;
+        flex-wrap: wrap;
+    }
+
+
+}
+
+@media (max-width: 576px) {
+    .product-search-wrapper {
+        flex-direction: column;
+        align-items: stretch;
+
+    }
+
+    .page-wrapper {
+        padding: 2rem;
+    }
+}
+
 /* ect */
 
 .min-w-10 {
-    min-width: 20rem;
+    min-width: 10rem;
 }
 
 .btn {
-    min-width: 4rem;
+    min-width: 2rem;
+}
+
+/* transition */
+
+.slide-fade-enter-active {
+    transition: all 0.3s ease-out;
+}
+
+.slide-fade-leave-active {
+    transition: all 0.3s ease-out;
+}
+
+.slide-fade-enter-from {
+    transform: translateY(-20px);
+    opacity: 0;
+}
+
+.slide-fade-leave-to {
+    transform: translateY(20px);
+    opacity: 0;
 }
 </style>
