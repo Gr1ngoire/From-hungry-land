26670df7d5e01a3f8156552835c52214ae07bd2d
Nazariy <nazar260804@gmail.com>
2023-02-05 02:20:57 +0200

    added store for product list page

---
+++ client/src/stores/productListStore.ts
@@ -0,0 +1,39 @@
+import type { ProductDTO, ProductTagDTO } from '@/common/types/types'
+import { ProductService, ProductTagService } from '@/services/services'
+import { defineStore } from 'pinia'
+import { ref, computed, type ComputedRef } from 'vue'
+
+
+export const useProductListStore = defineStore('products', () => {
+
+    const products = ref([] as ProductDTO[])
+    const productTags = ref([] as ProductTagDTO[])
+
+    const productService: ProductService = new ProductService();
+    const productTagService: ProductTagService = new ProductTagService();
+
+    const getProducts: ComputedRef<ProductDTO[]> = computed(() => products.value);
+    const getProductTags: ComputedRef<string[]> = computed(() => productTags.value.map((tag: ProductTagDTO) => tag.name));
+    const isProductsLoaded: ComputedRef<Boolean> = computed(() => products.value.length > 0);
+    const isProductTagsLoaded: ComputedRef<Boolean> = computed(() => productTags.value.length > 0);
+    
+    const fetchProducts = async () => {
+        products.value = await productService.getProducts()
+    }
+
+    const fetchProductTags = async () => {
+        productTags.value = await productTagService.getProductTags()    
+    }
+    
+    return {
+        products,
+        fetchProducts,
+        getProducts,
+        productTags,
+        fetchProductTags,
+        getProductTags
+    }
+
+
+
+})
