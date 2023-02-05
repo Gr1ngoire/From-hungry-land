50020eea53aaf35431f47013a5f71e939c791ce5
tonujet <tonujet@gmail.com>
2023-02-05 20:30:05 +0100

    Create RecipeDtoType

---
+++ shared/common/types/recipe/recipe.types.ts
@@ -0,0 +1,1 @@
+export type {RecipeDtoType} from "./recipe.dto.type"
--- shared/common/types/types.ts
+++ shared/common/types/types.ts
@@ -1,4 +1,5 @@
 export {type RoleResponseDto} from './role/role'
 export {type UserSignInDto, type UserSignInResponseDto, type UserSignUpDto, type UserResponseDto} from './user/user'
 export {type ProductTagDTO} from './productTag/productTag.types'
-export  {type ProductDTO} from './product/product.types'
+export {type ProductDTO} from './product/product.types'
+export {type RecipeDtoType} from "./recipe/recipe.dto.type"
