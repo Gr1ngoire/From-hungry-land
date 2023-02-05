f5d5f6a1e859f15068858fde54871067b45029d6
Nazariy <nazar260804@gmail.com>
2023-02-05 23:18:43 +0200

    added recipe route

--- client/src/common/enums/app/app-routes-enum.ts
+++ client/src/common/enums/app/app-routes-enum.ts
@@ -2,7 +2,8 @@ enum AppRoutes {
     ALL = "/:catchAll(.*)",
     SIGN_IN = "/signIn",
     SIGN_UP = "/signUp",
-    PRODUCTS = '/products'
+    PRODUCTS = '/products',
+    RECIPES_SINGLE = '/recipes/:id',
 }
 
 export {AppRoutes}
