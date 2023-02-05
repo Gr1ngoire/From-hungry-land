bfe0673a7eae03f1106b1c888b035f7935382a6a
Nazariy <nazar260804@gmail.com>
2023-02-05 02:19:56 +0200

    added types

---
+++ client/src/common/types/product/product.types.ts
@@ -0,0 +1,1 @@
+export type { ProductDTO } from './productDTO.type'
---
+++ client/src/common/types/types.ts
@@ -0,0 +1,2 @@
+export type {ProductTagDTO} from './productTag/productTag.types'
+export type {ProductDTO} from './product/product.types'
