7d6b320f638fd0be162b70fe7f74aba5ee880314
tonujet <tonujet@gmail.com>
2023-02-06 16:47:24 +0100

    Fix bugs

--- client/src/services/recipe/recipe.service.ts
+++ client/src/services/recipe/recipe.service.ts
@@ -1,16 +1,16 @@
-import {axios} from "../axios/axios";
+import {axiosService} from "../axios/axios";
 import {AppRoutes} from "@/common/enums/enums";
 import type {RecipeQueryOptionType} from "@/common/types/types";
 
 
 class RecipeService {
     public async getRecipe(id: number) {
-        const {data} = await axios.get(AppRoutes.RECIPE + id)
+        const {data} = await axiosService.get(AppRoutes.RECIPE + id)
         return data
     }
 
     public async getRecipes(options: RecipeQueryOptionType) {
-        const {data} = await axios.get(AppRoutes.RECIPES)
+        const {data} = await axiosService.get(AppRoutes.RECIPES)
         return data
     }
 }
