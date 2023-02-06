7b74c171bee3a9496350e457f1b561f21cdb2df8
tonujet <tonujet@gmail.com>
2023-02-06 01:12:45 +0100

    Recipe item logic

--- client/src/components/recipe-browser-view/recipe-item.vue
+++ client/src/components/recipe-browser-view/recipe-item.vue
@@ -1,14 +1,25 @@
 <script setup>
+import {RecipeDtoType} from "@/common/types/types"
+import type { PropType } from 'vue'
+
+const props = defineProps({
+  recipe: Object as PropType<RecipeDtoType>
+})
 
 </script>
 
 <template>
   <div class="item">
     <div class="item__img">
-
+      <img :src="props.recipe.imgUrl" :alt="props.recipe.name">
     </div>
     <div class="item__img-info">
-
+      <div class="item__difficult">
+        {{props.recipe.difficult}}
+      </div>
+      <div class="item__name">
+        {{props.recipe.name}}
+      </div>
     </div>
   </div>
 </template>
