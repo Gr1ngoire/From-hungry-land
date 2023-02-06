d91a4fd04b26a47b99e9bf6a2aadb8d13f2d9f1e
tonujet <tonujet@gmail.com>
2023-02-06 04:09:51 +0100

    Create difficulty option

--- client/src/components/recipe-browser-view/recipe-difficult.vue
+++ client/src/components/recipe-browser-view/recipe-difficult.vue
@@ -5,21 +5,17 @@ import {RecipeDifficulty} from "@/common/enums/enums";
 const props = defineProps({
   difficult: String as PropType<RecipeDifficulty>
 })
-// const determineDifficultClass = () =>{
-//   if(RecipeDifficulty.EASY){
-//     return "easy"
-//   }
-//   if(RecipeDifficulty.MEDIUM){
-//     return ""
-//   }
-// }
 
 </script>
+
+
 <template>
   <div :class="props.difficult">
     {{props.difficult}}
   </div>
 </template>
+
+
 <style scoped>
 .easy{
   color: #1cba16;
---
+++ client/src/components/recipe-browser-view/search-options/difficulty-option.vue
@@ -0,0 +1,58 @@
+<script setup lang="ts">
+import {$enum} from "ts-enum-util"
+import CheckBox from "@/common/components/CheckBox.vue";
+import RecipeDifficult from "@/components/recipe-browser-view/recipe-difficult.vue";
+import {RecipeDifficulty} from "@/common/enums/enums";
+import {ref} from "vue";
+
+const selectedDifficulties = ref({
+  "easy": false,
+  "medium": false,
+  "hard": false,
+})
+</script>
+
+<template>
+  <div class="option">
+    <div class="option__title">
+      Difficult
+    </div>
+    <div
+        class="option__raw"
+        v-for="difficult in $enum(RecipeDifficulty).getValues()"
+    >
+      <CheckBox
+          :id="difficult + 'difficultCheckBox'"
+          class="option__checkbox"
+          v-model="selectedDifficulties[difficult]"
+      />
+      <label :for="difficult + 'difficultCheckBox'">
+        <RecipeDifficult
+            :difficult="difficult"
+        />
+      </label>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.option__title {
+  font-size: 2rem;
+  margin-bottom: 8px;
+}
+
+.option__raw {
+  margin-left: 10px;
+  margin-bottom: 5px;
+}
+
+.option__raw {
+  display: flex;
+  align-items: center;
+
+}
+
+.option__checkbox {
+  margin-right: 8px;
+}
+</style>
