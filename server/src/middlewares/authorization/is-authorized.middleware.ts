072da9e7ee78cdb059da9829bfc2d1456e8f8c73
tonujet <tonujet@gmail.com>
2023-02-07 00:16:12 +0100

    Decomposed logic of authorisation validation to function

--- server/src/middlewares/authorization/is-authorized.middleware.ts
+++ server/src/middlewares/authorization/is-authorized.middleware.ts
@@ -1,24 +1,10 @@
 import {NextFunction, Request, RequestHandler, Response} from "express";
-import {jwtService} from "@/services/services";
-import {ValidationExceptionMessages} from "shared/common/enums/exception/validation/validation-exception-message.enum";
-import { UnauthorizedException } from "@/common/exceptions/exceptions";
+import {isAuthorizedFunction} from "@/middlewares/authorization/is-authorized.function";
 
 const isAuthorized: RequestHandler = async (req: Request, res: Response, next: NextFunction): Promise<void> => {
     const authHeader = req.headers.authorization;
-    const [tokenFormat, token] = authHeader.split(' ')
-
-    if (tokenFormat !== 'Bearer' || !token) {
-        next(new UnauthorizedException(ValidationExceptionMessages.USER_IS_UNAUTHORIZED));
-    }
-
-    try {
-        jwtService.verifyToken(token);
-        next();
-    } catch {
-        next(new UnauthorizedException(ValidationExceptionMessages.USER_IS_UNAUTHORIZED));
-    }
-
-
+    isAuthorizedFunction(authHeader)
+    next()
 }
 
 export {isAuthorized}
