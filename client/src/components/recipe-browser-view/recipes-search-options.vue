5c655e180a5b9dcbd4f45a557c01d8f94b03d595
tonujet <tonujet@gmail.com>
2023-02-06 03:45:10 +0100

    Create possible recipe options

--- client/src/components/recipe-browser-view/recipes-search-options.vue
+++ client/src/components/recipe-browser-view/recipes-search-options.vue
@@ -1,20 +1,53 @@
-<script setup>
+<script setup lang="ts">
 
+
+
+
+import PossibleRecipesOption from "@/components/recipe-browser-view/search-options/possible-recipes-option.vue";
+import DifficultyOption from "@/components/recipe-browser-view/search-options/difficulty-option.vue";
 </script>
 
 
 <template>
   <div class="search-options page-section">
     <div class="search-options__header">
-      asdsa
+      <div class="search-options__title">
+        <h2>Search options</h2>
+      </div>
+      <div class="search-options__logo">
+        <img src="https://cdn-icons-png.flaticon.com/512/73/73391.png" alt="options">
+      </div>
     </div>
     <div class="search-options__body">
-      sadsad
+      <PossibleRecipesOption/>
+      <DifficultyOption/>
     </div>
   </div>
 </template>
 
 
 <style scoped>
+.search-options__header{
+  display: flex;
+  justify-content: space-between;
+  margin-bottom: 50px;
+}
+.search-options__title{
+  margin-top: 20px;
+}
+.search-options__logo{
+  margin-top: 5px;
+  width: 70px;
+  height: 70px;
+}
+.search-options__logo img{
+  width: 100%;
+  height: 100%;
+}
+.search-options__body{
 
+}
+h2{
+  margin: 0;
+}
 </style>
---
+++ client/src/components/recipe-browser-view/search-options/possible-recipes-option.vue
@@ -0,0 +1,31 @@
+<script setup lang="ts">
+import CheckBox from "@/common/components/CheckBox.vue";
+import {ref} from "vue";
+
+const isSelected = ref<boolean>(false)
+const checkBoxId:string = "PossibleRecipesCheckBox"
+</script>
+
+<template>
+  <div class="option">
+    <CheckBox
+        class="option__checkbox"
+        v-model="isSelected"
+        :id="checkBoxId"
+    />
+    <label :for="checkBoxId">
+      Possible recipes for your product list
+    </label>
+
+  </div>
+</template>
+
+<style scoped>
+.option {
+  display: flex;
+  align-items: center;
+}
+.option__checkbox{
+  margin-right: 20px;
+}
+</style>
