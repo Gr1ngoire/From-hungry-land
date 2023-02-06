02db64df0b5c008b16a2216fbf6f83659ac57873
tonujet <tonujet@gmail.com>
2023-02-06 02:23:12 +0100

    Create recipe-difficult.vue

---
+++ client/src/components/recipe-browser-view/recipe-difficult.vue
@@ -0,0 +1,33 @@
+<script setup lang="ts">
+import type {PropType} from "vue";
+import {RecipeDifficulty} from "@/common/enums/enums";
+
+const props = defineProps({
+  difficult: String as PropType<RecipeDifficulty>
+})
+// const determineDifficultClass = () =>{
+//   if(RecipeDifficulty.EASY){
+//     return "easy"
+//   }
+//   if(RecipeDifficulty.MEDIUM){
+//     return ""
+//   }
+// }
+
+</script>
+<template>
+  <div :class="props.difficult">
+    {{props.difficult}}
+  </div>
+</template>
+<style scoped>
+.easy{
+  color: #1cba16;
+}
+.medium{
+  color: #FF9900;
+}
+.hard{
+  color: #FF0000;
+}
+</style>
