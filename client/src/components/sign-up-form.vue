eab1980ef2baa7cfed904a53bd269f45e9c665fc
wilhelm wesser <wilhelmwesser228@gmail.com>
2023-02-07 09:44:59 +0200

    Logout is done

--- client/src/components/sign-up-form.vue
+++ client/src/components/sign-up-form.vue
@@ -53,7 +53,6 @@ const handleSubmit: (event: Event) => void = (event: Event) => {
   event.preventDefault();
   if (Object.values(signUpValidationState).every((el) => el.length === 0)) {
     authStore.signUp(signUpFormState);
-    console.log("SIGN UP!", signUpFormState)
     router.push(AppRoutes.PRODUCTS);
   }
 };
