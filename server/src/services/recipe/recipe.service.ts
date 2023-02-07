3797958944f9f8e166ef5fcac5e759fe98180e77
Nazariy <nazar260804@gmail.com>
2023-02-07 19:26:47 +0200

    service & controller for checking ingredients

--- server/src/services/recipe/recipe.service.ts
+++ server/src/services/recipe/recipe.service.ts
@@ -62,13 +62,14 @@ export class RecipeService {
         const recipes: Recipe[] = await this.repo.getAllWithRelations()
         const possibleRecipes:Recipe[] = []
         const user = await this.userService.getByIdWithProducts(userId)
-
+    
         for (const recipe of recipes) {
             let isPossible = true
-            for (const product of recipe.productRecipes) {
-                const isExist = user.products.find((userProduct) => userProduct.id === product.id);
+            for (const productRecipe of recipe.productRecipes) {
+                const isExist = user.products.find((userProduct) => userProduct.id === productRecipe.product.id );
                 if(!isExist){
                     isPossible = false
+                    console.log("Break")
                     break
                 }
             }
@@ -77,6 +78,18 @@ export class RecipeService {
             }
         }
         return possibleRecipes
+    }
+
+    async checkRecipeIngredients(userId: number, recipeId: number) {
+        const recipe = await this.repo.get(recipeId)
+        const user = await this.userService.getByIdWithProducts(userId)
+        let possibleIngredients: {id: number, isPresent: boolean}[] = []
+        for (const productRecipe of recipe.productRecipes) {
+            const isExist = user.products.find((userProduct) => userProduct.id === productRecipe.product.id );
+            possibleIngredients.push({id: productRecipe.product.id, isPresent: Boolean(isExist)})
+        }
+        return possibleIngredients
+
     }
 
 }
