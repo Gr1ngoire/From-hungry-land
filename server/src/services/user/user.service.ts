37834da68e82224d66939e97821130929888cce6
Nazariy <nazar260804@gmail.com>
2023-02-07 03:07:49 +0200

    added function to get user products

--- server/src/services/user/user.service.ts
+++ server/src/services/user/user.service.ts
@@ -1,5 +1,6 @@
 import {UserRepository} from "@/repositories/repositories";
 import {type UserResponseDto, type UserSignUpDto, UserWithPassword} from "@/common/types/types";
+import { UserProductsDTO } from "shared/common/types/user/user-products-dto.type";
 
 class UserService {
     constructor(private repository: UserRepository) {}
@@ -18,6 +19,10 @@ class UserService {
 
     public create(userDto: UserSignUpDto): Promise<UserResponseDto> {
         return this.repository.create(userDto);
+    }
+
+    public async getUserProducts(id: number): Promise<UserProductsDTO> {
+        return await this.repository.getUserProducts(id);
     }
 }
 
