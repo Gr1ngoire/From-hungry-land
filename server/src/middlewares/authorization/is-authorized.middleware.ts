9eaffeff438e0ade3b7e4ac623a5005c9c1cbe83
tonujet <tonujet@gmail.com>
2023-02-07 11:04:05 +0100

    Fix authorization bugs

--- server/src/middlewares/authorization/is-authorized.middleware.ts
+++ server/src/middlewares/authorization/is-authorized.middleware.ts
@@ -2,9 +2,14 @@ import {NextFunction, Request, RequestHandler, Response} from "express";
 import {isAuthorizedFunction} from "@/middlewares/authorization/is-authorized.function";
 
 const isAuthorized: RequestHandler = async (req: Request, res: Response, next: NextFunction): Promise<void> => {
-    const authHeader = req.headers.authorization;
-    isAuthorizedFunction(authHeader)
-    next()
+    try{
+        const authHeader = req.headers.authorization;
+        isAuthorizedFunction(authHeader)
+        next()
+    }catch (e){
+        next(e)
+    }
+
 }
 
 export {isAuthorized}
