1f58b5f409395215dd917875088466a5df96524b
Nazariy <nazar260804@gmail.com>
2023-02-07 04:20:16 +0200

    methods and getters for filtering and search

--- client/src/stores/productList.store.ts
+++ client/src/stores/productList.store.ts
@@ -9,14 +9,17 @@ export const useProductListStore = defineStore(StoreNames.PRODUCTS, () => {
 
     const products = ref([] as ProductDTO[])
     const productTags = ref([] as ProductTagDTO[])
+    const searchProducts = ref<ProductDTO[]>([])
 
     const productService: ProductService = new ProductService();
     const productTagService: ProductTagService = new ProductTagService();
 
     const getProducts: ComputedRef<ProductDTO[]> = computed(() => products.value);
     const getProductTags: ComputedRef<string[]> = computed(() => productTags.value.map((tag: ProductTagDTO) => tag.name));
+    const getSearchProducts: ComputedRef<ProductDTO[]> = computed(() => searchProducts.value);
     const isProductsLoaded: ComputedRef<Boolean> = computed(() => products.value.length > 0);
     const isProductTagsLoaded: ComputedRef<Boolean> = computed(() => productTags.value.length > 0);
+
     
     const fetchProducts = async (options?: {
         take?: number,
@@ -34,17 +37,29 @@ export const useProductListStore = defineStore(StoreNames.PRODUCTS, () => {
         )
     }
 
+    const fetchSearchProducts = async (name?: string) => {
+        searchProducts.value = await productService.getProducts({ name: name, all: true})
+    }
+
+    const filterProducts = (name?: string, filters?: string[] | string) => {
+        fetchProducts({ name, filters })
+    }
+
     const fetchProductTags = async () => {
         productTags.value = await productTagService.getProductTags()    
     }
     
     return {
-        products,
         fetchProducts,
         getProducts,
         productTags,
         fetchProductTags,
-        getProductTags
+        getProductTags,
+        isProductsLoaded,
+        isProductTagsLoaded,
+        filterProducts,
+        fetchSearchProducts,
+        getSearchProducts
     }
 
 
